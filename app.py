f21065b17af0240cdb0de856f4180e7721a724f7
Ezequiel Bengoechea <eze.bengo224@gmail.com>
2026-03-28 11:30:59 -0300

    feat: agregar reintento de conexion con la base de datos

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 from flask import Flask, render_template
 import mysql.connector
+import time
 
 app = Flask(__name__)
 
+def connect_with_retry():
+    retries = 10
+    delay = 3
+
+    for i in range(retries):
+        try:
+            db = mysql.connector.connect(
+                host="mysql",
+                user="root",
+                password="root",
+                database="testdb",
+                charset="utf8mb4"
+            )
+            print("✅ Conectado a MySQL")
+            return db
+        except Exception as e:
+            print(f"⏳ Intento {i+1}/{retries} - esperando DB...")
+            time.sleep(delay)
+
+    raise Exception("❌ No se pudo conectar a la base de datos")
+
 @app.route("/")
 def home():
-    db = mysql.connector.connect(
-        host="mysql",
-        user="root",
-        password="root",
-        database="testdb",
-        charset="utf8mb4"
-    )
+    db = connect_with_retry()  # 👈 cambio clave
     cursor = db.cursor()
 
     cursor.execute("SELECT id, description FROM items")
